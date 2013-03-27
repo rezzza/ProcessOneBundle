fbf6543d7baa7a6d9d549401c535ce63cd9b023c
Stéphane PY <py.stephane1@gmail.com>
2013-03-27 18:01:03 +0100

    Update phpdoc of APSMessage

--- Message/APSMessage.php
+++ Message/APSMessage.php
@@ -5,6 +5,9 @@ namespace Rezzza\ProcessOneBundle\Message;
 /**
  * APSMessage 
  *
+ * Apple documentation of Apple Push Notification Service.
+ * http://developer.apple.com/library/ios/#documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/ApplePushService/ApplePushService.html#//apple_ref/doc/uid/TP40008194-CH100-SW1
+ *
  * @uses AbstractMessage
  * @uses MessageInterface
  * @author Stephane PY <py.stephane1@gmail.com> 
